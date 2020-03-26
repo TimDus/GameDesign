013a55548469079f775736aa75294eb8095736fe
TimDus <timvddussen@gmail.com>
2020-03-26 01:53:27 +0100

    Infinite Road

---
+++ InfiniteRoad/Assets/Roadspawn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roadspawn : MonoBehaviour
+{
+    public GameObject prefab;
+    private GameObject lastPlatform;
+    private GameObject prevPlatform;
+    public bool check;
+    public bool ready;
+    private int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+        GameObject[] platform = GameObject.FindGameObjectsWithTag("Road");
+        foreach (GameObject h in platform)
+        {
+            lastPlatform = h;
+        }
+        StartCoroutine(coroutine());
+    }
+    IEnumerator coroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        yield return new WaitUntil(() => lastPlatform.GetComponentInChildren<SpawnNew>().Ready());
+        spawn();
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        StartCoroutine(coroutine());
+    }
+    void spawn()
+    {
+        i++;
+        prevPlatform = lastPlatform;
+        lastPlatform = Instantiate(prefab, lastPlatform.transform.GetChild(0).position, Quaternion.identity);
+        lastPlatform.name = "Road" + i.ToString();
+    }
+
+}
---
+++ InfiniteRoad/Assets/SpawnNew.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNew : MonoBehaviour
+{
+    public bool spawnReady;
+    public bool ready;
+    private void Start()
+    {
+        spawnReady = false;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        spawnReady = true;
+    }
+    public bool Ready()
+    {
+        return spawnReady;
+    }
+}
